bebb8de9d2490f9846afd1a0ce44b685892718bf
szymonpl200 <deadriper.piotrowki@gmail.com>
2020-04-05 20:14:31 +0200

    Basic App v2

--- rss/src/main/java/com/rssapp/rss/config/WebMvcConfig.java
+++ rss/src/main/java/com/rssapp/rss/config/WebMvcConfig.java
@@ -1,8 +1,6 @@
 package com.rssapp.rss.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,14 +10,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer 
 {
-	@Bean
-	public BCryptPasswordEncoder passwordEncoder() 
-	{
-		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
-		
-		return bCryptPasswordEncoder;
-	}
-	
 	@Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler(
--- rss/src/main/java/com/rssapp/rss/controller/AppController.java
+++ rss/src/main/java/com/rssapp/rss/controller/AppController.java
@@ -3,17 +3,31 @@ package com.rssapp.rss.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+
+import com.rssapp.rss.entity.RssFeed;
 
 @Controller
 public class AppController {
 	
-	@RequestMapping(value={"/", "/home"}, method = RequestMethod.GET)
-	public ModelAndView login(){
-		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.setViewName("home");
-		
-		return modelAndView;
-	}
+	@RequestMapping(value={"/", "/login"}, method = RequestMethod.GET)
+    public ModelAndView login(@RequestParam(value = "error", required = false) String error){
+        ModelAndView modelAndView = new ModelAndView();
+        if (error != null) 
+        {
+          modelAndView.setViewName("error page");
+        } 
+        else
+        {
+    		RssFeed rssFeed = new RssFeed();
+    		modelAndView.addObject("rssFeed", rssFeed);
+    		modelAndView.setViewName("home");
+    		
+        }
+
+        return modelAndView;
+    }
 
 }
